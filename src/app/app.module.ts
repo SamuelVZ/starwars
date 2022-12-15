d34be2a491afd67f181666b3e778c0f02b2c1c5c
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-15 11:48:34 -0800

    list componen created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './components/tabs/tabs.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
