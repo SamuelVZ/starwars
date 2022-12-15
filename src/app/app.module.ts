ef0a36569b7e82d75f831d8bd400ed9c2ba9c006
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-15 11:49:19 -0800

    item component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { ListComponent } from './components/list/list.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabsComponent,
-    ListComponent
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
