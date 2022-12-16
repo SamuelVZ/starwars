f34a6c2ed3d11f09c739136e29deeaf58f18235a
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-16 14:16:53 -0800

    calling the initial request on Appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarWarsService } from './services/star-wars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private starwarsService: StarWarsService) {}
+
+  ngOnInit(): void {
+    this.starwarsService.fetchCharacters().subscribe((data) => {
+      data.results.map((char: { name: any }) => {
+        // const fetchChar: Character = { name: char.name };
+        this.starwarsService.addCharacter(char.name, '');
+      });
+    });
+  }
   title = 'starwars';
 }
