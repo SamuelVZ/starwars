9f89429687d9d1faf778bbbe5650a911f5932898
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-16 10:31:20 -0800

    on submit method created, service injected

--- src/app/components/create-character/create-character.component.ts
+++ src/app/components/create-character/create-character.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { StarWarsService } from '../../services/star-wars.service';
 
 @Component({
   selector: 'app-create-character',
   templateUrl: './create-character.component.html',
-  styleUrls: ['./create-character.component.css']
+  styleUrls: ['./create-character.component.css'],
 })
 export class CreateCharacterComponent implements OnInit {
+  starwarsService: StarWarsService;
+  availableSides = [
+    { display: 'None', side: '' },
+    { display: 'Light', side: 'light' },
+    { display: 'Dark', side: 'dark' },
+  ];
+  defaultName = 'Obi-Wan';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(starwarsService: StarWarsService) {
+    this.starwarsService = starwarsService;
   }
 
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(form);
+    this.starwarsService.addCharacter(form.value.name, form.value.side);
+  }
 }
