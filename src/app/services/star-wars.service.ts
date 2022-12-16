774eaaa981ae64c5cf91c846df7442b256c3e49e
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-16 10:28:23 -0800

    add character method added

--- src/app/services/star-wars.service.ts
+++ src/app/services/star-wars.service.ts
@@ -3,34 +3,42 @@ import { Character } from '../Models/Character';
 import { LogService } from './log.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StarWarsService {
   private logService: LogService;
 
   constructor(logService: LogService) {
     this.logService = logService;
-   }
+  }
 
   private characters: Character[] = [
-    {name: 'Luke', side: ''},
-    {name: 'Darth Vader', side: ''}
+    { name: 'Luke', side: '' },
+    { name: 'Darth Vader', side: '' },
   ];
 
-
-  getCharacters(choosenList: string): Character[]{
-
-    if(choosenList === 'all'){
+  getCharacters(choosenList: string): Character[] {
+    if (choosenList === 'all') {
       return this.characters.slice(); //slice returns a copy of the array
     }
 
-    return this.characters.filter(c => c.side === choosenList);
+    return this.characters.filter((c) => c.side === choosenList);
   }
 
-
-  onSideAssigned(char: Character){
-    const pos = this.characters.findIndex(c => c.name === char.name);
+  onSideAssigned(char: Character) {
+    const pos = this.characters.findIndex((c) => c.name === char.name);
     this.characters[pos].side = char.side;
-    this.logService.writeLog('changed side of ' + char.name + ' to be: ' + char.side);
+    this.logService.writeLog(
+      'changed side of ' + char.name + ' to be: ' + char.side
+    );
+  }
+
+  addCharacter(newName: string, newSide: string) {
+    const pos = this.characters.findIndex((c) => c.name === newName);
+    if (pos !== -1) {
+      return;
+    }
+    const newCharacter: Character = { name: newName, side: newSide };
+    this.characters.push(newCharacter);
   }
 }
