afc01d3180d5f1faaca6e97d5093707c463560f3
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-12-16 12:00:54 -0800

    tab component refractored to use routeroutlet with childs

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,33 +1,12 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
-import { Character } from '../../Models/Character';
-import { StarWarsService } from '../../services/star-wars.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.css']
+  styleUrls: ['./tabs.component.css'],
 })
 export class TabsComponent implements OnInit {
+  constructor() {}
 
-  characters!: Character[];
-  choosenList = 'all';
-  starWarsService: StarWarsService;
-
-
-  constructor(starWarsService: StarWarsService) {
-    this.starWarsService = starWarsService;
-   }
-
-  ngOnInit(): void {
-  }
-
-  getCharacters(): Character[]{
-    this.characters = this.starWarsService.getCharacters(this.choosenList);
-    return this.characters;
-  }
-
-  onChoose(sideChoosen: string){
-    this.choosenList = sideChoosen;
-  }
-
+  ngOnInit(): void {}
 }
